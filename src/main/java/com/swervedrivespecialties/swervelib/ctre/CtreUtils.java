c32df8be34eae6e321ba252070cf28055bce9cd8
awtpi314 <awtpi314@gmail.com>
2021-12-27 12:33:25 -0600

    added back error checking on the robot hardware

--- src/main/java/com/swervedrivespecialties/swervelib/ctre/CtreUtils.java
+++ src/main/java/com/swervedrivespecialties/swervelib/ctre/CtreUtils.java
@@ -2,13 +2,17 @@ package com.swervedrivespecialties.swervelib.ctre;
 
 import com.ctre.phoenix.ErrorCode;
 
+import edu.wpi.first.wpilibj.RobotBase;
+
 public final class CtreUtils {
     private CtreUtils() {
     }
 
     public static void checkCtreError(ErrorCode errorCode, String message) {
-        if (errorCode != ErrorCode.OK) {
-            System.err.println("Could not update motor: Assuming we are running in the simulator...");
+        if (errorCode != ErrorCode.OK && RobotBase.isReal()) {
+            throw new RuntimeException(String.format("%s: %s", message, errorCode.toString()));
+        } else if (!RobotBase.isReal()) {
+            System.err.println("Motor config failed because running in simulator");
         }
     }
 }
